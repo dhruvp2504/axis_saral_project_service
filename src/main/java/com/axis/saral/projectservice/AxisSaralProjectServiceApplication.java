45865e4ec7f086a9874dbecf7e17d660cef798bb
dhruv2504 <dhruv2504@gmail.com>
2023-01-11 11:24:29 +0530

    Project Service Created

---
+++ src/main/java/com/axis/saral/projectservice/AxisSaralProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.axis.saral.projectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisSaralProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxisSaralProjectServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axis/saral/projectservice/AxisSaralProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axis.saral.projectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxisSaralProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
